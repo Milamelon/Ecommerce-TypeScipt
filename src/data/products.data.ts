c3b9f73ceac95e9622156de97e8f224fc40a4939
Milamelon <fresitaryu@gmail.com>
2026-07-22 19:06:46 -0600

    agregar: Tipos de producto y datos estaticos con 25 productos

---
+++ src/data/products.data.ts
@@ -0,0 +1,233 @@
+import {Product} from "@/types/product.types";
+
+export const products: Product[] = [
+
+
+    {
+        "id": 1,
+        "title": "Tres Leches Clásico",
+        "description": "Bizcocho ultra esponjoso bañado artesanalmente en nuestra receta secreta de tres leches, coronado con una nube de merengue flameado y un sutil toque de canela.",
+        "price": 28.00,
+        "image": "/images/3leches.png",
+        "category": "Pasteles y Tortas",
+        "quantity": 10
+    },
+    {
+        "id": 2,
+        "title": "Tarta de Queso New York",
+        "description": "Auténtica tarta de queso horneada, con una textura increíblemente cremosa y densa sobre una base crujiente de galleta graham, cubierta con un vibrante coulis de frutos rojos.",
+        "price": 34.00,
+        "image": "/images/tartaqueso.jpg",
+        "category": "Pasteles y Tortas",
+        "quantity": 10
+    },
+    {
+        "id": 3,
+        "title": "Tarta Opera",
+        "description": "Elegante clásico francés de capas milimétricas de bizcocho de almendra (joconde) empapado en café espresso, intercalado con crema de mantequilla de café y una sedosa ganache de chocolate oscuro.",
+        "price": 4.50,
+        "image": "/images/opera.jpg",
+        "category": "Pasteles y Tortas",
+        "quantity": 10
+    },
+    {
+        "id": 4,
+        "title": "Red Velvet",
+        "description": "Bizcocho aterciopelado de un rojo vibrante con un toque sutil de cacao, perfectamente equilibrado con capas generosas de un suave y cremoso betún de queso crema.",
+        "price": 30.00,
+        "image": "/images/rv.jpg",
+        "category": "Pasteles y Tortas",
+        "quantity": 10
+    },
+    {
+        "id": 5,
+        "title": "Selva Negra",
+        "description": "Capas de bizcocho de chocolate húmedo embebido en licor de cerezas, rellenas de crema batida premium y cerezas ácidas, decorado con virutas de chocolate oscuro.",
+        "price": 32.00,
+        "image": "/images/sn.jpg",
+        "category": "Pasteles y Tortas",
+        "quantity": 10
+    },
+    {
+        "id": 6,
+        "title": "Flan de Queso Premium",
+        "description": "Postre tradicional horneado lentamente al baño María para lograr una textura sedosa que se derrite en la boca, bañado en un espejo de caramelo líquido dorado.",
+        "price": 3.00,
+        "image": "/images/flanqueso.JPG",
+        "category": "Postres Frios",
+        "quantity": 10
+    },
+    {
+        "id": 7,
+        "title": "Tiramisú Original",
+        "description": "Delicadas plantillas húmedas en café espresso cargado y licor de café, dispuestas en capas con una rica y auténtica crema de queso mascarpone, espolvoreado con cacao puro en polvo.",
+        "price": 4.50,
+        "image": "/images/tiramisu.jpg",
+        "category": "Postres Frios",
+        "quantity": 10
+    },
+    {
+        "id": 8,
+        "title": "Panna Cotta de Vainilla",
+        "description": "Delicadeza italiana a base de crema y gelatina, infusionada con vainilla natural en vaina y servida con un exótico y refrescante coulis de mango y maracuyá.",
+        "price": 3.75,
+        "image": "/images/pana.jpg",
+        "category": "Postres Frios",
+        "quantity": 10
+    },
+    {
+        "id": 9,
+        "title": "Mousse de Chocolate Oscuro",
+        "description": "Una tentación ligera y aireada de textura perfecta, elaborada con chocolate de origen con 70% de cacao, ideal para los verdaderos amantes del chocolate.",
+        "price": 3.50,
+        "image": "/images/mousse.jpg",
+        "category": "Postres Frios",
+        "quantity": 10
+    },
+    {
+        "id": 10,
+        "title": "Crème Brûlée",
+        "description": "Crema pastelera rica y sedosa aromatizada con vainilla, cuya superficie es espolvoreada con azúcar y sopleteada al momento para crear una capa de caramelo cristalino y crujiente.",
+        "price": 4.25,
+        "image": "/images/creme.jpg",
+        "category": "Postres Frios",
+        "quantity": 10
+    },
+    {
+        "id": 11,
+        "title": "Macarons Franceses",
+        "description": "Delicados bocadillos de merengue de almendra con una fina cubierta crujiente e interior tierno, rellenos de exquisitas ganaches y curds de frutas de temporada.",
+        "price": 8.50,
+        "image": "/images/maca.jpg",
+        "category": "Galletas y Pasteleria Individual",
+        "quantity": 10
+    },
+    {
+        "id": 12,
+        "title": "Brownies de Fudge con Nueces",
+        "description": "Cuadrados de chocolate intensos, densos y melcochosos (fudgy), cargados con trozos de nueces tostadas que aportan el toque crujiente perfecto.",
+        "price": 2.50,
+        "image": "/images/brownie.jpg",
+        "category": "Galletas y Pasteleria Individual",
+        "quantity": 10
+    },
+    {
+        "id": 13,
+        "title": "Profiteroles de Crema",
+        "description": "Delicada masa choux horneada, hueca y crujiente, rellena de una tersa crema pastelera tradicional y bañada con hilos calientes de chocolate fundido.",
+        "price": 3.25,
+        "image": "/images/potri.jpg",
+        "category": "Galletas y Pasteleria Individual",
+        "quantity": 10
+    },
+    {
+        "id": 14,
+        "title": "Galletas con Chispas de Chocolate",
+        "description": "La receta clásica perfecta: galletas doradas y crujientes en los bordes, con un centro suave y masticable repleto de chispas de chocolate semi-amargo que se derriten.",
+        "price": 1.75,
+        "image": "/images/chispas.jpg",
+        "category": "Galletas y Pasteleria Individual",
+        "quantity": 10
+    },
+    {
+        "id": 15,
+        "title": "Alfajores de Maicena",
+        "description": "Galletas ultra delicadas que se deshacen en la boca al primer bocado, unidas por un abundante y tradicional relleno de dulce de leche, decoradas con coco rallado.",
+        "price": 6.00,
+        "image": "/images/alfa.jpg",
+        "category": "Galletas y Pasteleria Individual",
+        "quantity": 10
+    },
+    {
+        "id": 16,
+        "title": "Pay de Limón con Merengue",
+        "description": "Base de masa quebrada crujiente rellena de una crema de limón intensamente cítrica y equilibrada, coronada con majestuosos picos de merengue suizo tostado.",
+        "price": 25.00,
+        "image": "/images/plimon.jpg",
+        "category": "Tartas y pies",
+        "quantity": 10
+    },
+    {
+        "id": 17,
+        "title": "Tarta de Manzana Clásica",
+        "description": "Relleno reconfortante y especiado de manzanas picadas con canela y azúcar, horneado dentro de una masa hojaldrada, dorada y crujiente.",
+        "price": 26.00,
+        "image": "/images/pmanzana.png",
+        "category": "Tartas y pies",
+        "quantity": 10
+    },
+    {
+        "id": 18,
+        "title": "Tarta de Santiago",
+        "description": "Postre tradicional gallego elaborado a base de almendras molidas, huevo y azúcar (naturalmente sin gluten), horneado a la perfección y decorado con la silueta de la Cruz de Santiago.",
+        "price": 3.50,
+        "image": "/images/tsantiago.jpg",
+        "category": "Tartas y pies",
+        "quantity": 10
+    },
+    {
+        "id": 19,
+        "title": "Pay de Nuez Pecana",
+        "description": "Una tarta rica y otoñal con un relleno cremoso de jarabe de maple y mantequilla, cubierta densamente con una capa de nueces pecanas enteras y tostadas.",
+        "price": 30.00,
+        "image": "/images/pnuez.jpg",
+        "category": "Tartas y pies",
+        "quantity": 10
+    },
+    {
+        "id": 20,
+        "title": "Tarta de Frutas de la Estación",
+        "description": "Base de masa sableada crujiente con una fina capa de crema pastelera de vainilla, decorada artísticamente con una selección de frutas frescas y brillantes del día.",
+        "price": 28.00,
+        "image": "/images/tfrutas.jpg",
+        "category": "Tartas y pies",
+        "quantity": 10
+    },
+    {
+        "id": 21,
+        "title": "Gelato de Pistacho Artesanal",
+        "description": "Helado de estilo italiano, notablemente más denso, cremoso y con menos grasa que el helado tradicional, elaborado con pasta pura de pistachos tostados.",
+        "price": 3.50,
+        "image": "/images/pistacho.jpg",
+        "category": "Helados y Postres Congelados",
+        "quantity": 10
+    },
+    {
+        "id": 22,
+        "title": "Baked Alaska Individual",
+        "description": "Una obra de arte congelada. Una base de bizcocho tierno con una bola de helado premium por dentro, cubierto completamente por merengue italiano que se flamea en el momento de servir.",
+        "price": 4.75,
+        "image": "/images/alaska.jpg",
+        "category": "Helados y Postres Congelados",
+        "quantity": 10
+    },
+    {
+        "id": 23,
+        "title": "Sorbete de Maracuyá",
+        "description": "Postre congelado ultra refrescante hecho a base de agua purificada, azúcar y la pulpa ácida y aromática del fruto de la pasión (100% libre de lácteos).",
+        "price": 3.00,
+        "image": "/images/maracuya.jpg",
+        "category": "Helados y Postres Congelados",
+        "quantity": 10
+    },
+    {
+        "id": 24,
+        "title": "Semifrío de Turrón",
+        "description": "Postre helado de origen italiano con una textura suave y aireada similar a una mousse congelada, entremezclado con trozos crujientes de turrón de almendra.",
+        "price": 4.00,
+        "image": "/images/semi.jpg",
+        "category": "Helados y Postres Congelados",
+        "quantity": 10
+    },
+    {
+        "id": 25,
+        "title": "Parfait de Café y Amaretto",
+        "description": "Capas congeladas y cremosas de parfait aromatizado con café espresso y un toque de licor de Amaretto, intercaladas con migas de galleta amaretti para una experiencia gourmet.",
+        "price": 4.25,
+        "image": "/images/cafe.jpg",
+        "category": "Helados y Postres Congelados",
+        "quantity": 10
+    }
+
+
+];
