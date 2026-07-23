6cc2c0102b4329e99f0050fe68ba820cee9fda29
Milamelon <fresitaryu@gmail.com>
2026-07-22 20:42:05 -0600

    agregar: type de usuarios con un usuario de ejemplo

---
+++ src/data/users.data.ts
@@ -0,0 +1,10 @@
+import {User} from '@/types/user.type';
+
+export const usersSeed: User[] = [
+  {
+    id: 1,
+    fullName: 'Mila',
+    email: 'mila@example.com',
+    password: '12345',
+  },
+];
